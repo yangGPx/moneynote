6f2778ec6550d1b5b4fe2432f407d49f51ea4377
yangGPx <1299563700@qq.com>
2021-04-21 15:16:05 +0800

    window挂载太多全局变量，容易重名，故全挂载到store上

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -12,12 +12,14 @@
   }
 
   interface Window {
-    tagList: Tag[]
-    tagRemove: (id: number|string) => boolean
-    tagUpdate: (id: number|string, name: string) => 'success' | 'duplicated' | 'empty' | 'error'
-    tagGetOne: (id: number|string) => Tag
-    tagCreate: (name:string) => 'success' | 'duplicated' | 'empty'
-    recordList: RecordItem[]
-    recordCreate: (item: RecordItem) => boolean
-    recordSave: () => void
+    store:{
+      tagList: Tag[]
+      tagRemove: (id: number|string) => boolean
+      tagUpdate: (id: number|string, name: string) => 'success' | 'duplicated' | 'empty' | 'error'
+      tagGetOne: (id: number|string) => Tag
+      tagCreate: (name:string) => 'success' | 'duplicated' | 'empty'
+      recordList: RecordItem[]
+      recordCreate: (item: RecordItem) => boolean
+      recordSave: () => void
+    }
   }
--- src/main.ts
+++ src/main.ts
@@ -15,32 +15,35 @@ Vue.component('icon', Icon)
 
 // 利用window做全局状态管理 
 // 在小项目中体现不出来，大项目多人协作开发中，如果同一份数据，有多种获取、操作数据的方法，可能导致不同数据不统一
-window.tagList = TagModel.fetch();
-window.recordList = RecordModel.fetch()
 
-window.tagRemove = function(id) {
-  return TagModel.delete(id);
+window.store = {
+  tagList:TagModel.fetch(),
+  recordList :RecordModel.fetch(),
+  tagRemove = function(id) {
+    return TagModel.delete(id);
+  },
+  tagUpdate: function(id, name) {
+    return TagModel.update(id, name);
+  },
+  
+  tagGetOne: function (id) {
+    return TagModel.getOneById(id);
+  },
+  
+  tagCreate: function (name) {
+    return TagModel.createTag(name);
+  },
+  
+  recordCreate = function(item) {
+    return RecordModel.create(item);
+  },
+  
+  recordSave:= function() {
+    RecordModel.save();
+  },
 }
 
-window.tagUpdate = function(id, name) {
-  return TagModel.update(id, name);
-}
 
-window.tagGetOne = function (id) {
-  return TagModel.getOneById(id);
-}
-
-window.tagCreate = function (name) {
-  return TagModel.createTag(name);
-}
-
-window.recordCreate = function(item) {
-  return RecordModel.create(item);
-}
-
-window.recordSave = function() {
-  RecordModel.save();
-}
 new Vue({
   router,
   store,
